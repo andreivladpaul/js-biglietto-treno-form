74600979ad8b775bd51cc47c60d8a0aa181e79ff
Vlad <andreivladpaul@gmail.com>
2021-07-22 15:25:21 +0200

    html + css + parte js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var btnGenera = document.getElementById("calcolaPrezzo");
+
+btnGenera.addEventListener("click",
+    function() {
+       /*  var nome = document.getElementById("nome").value;
+        var km = document.getElementById("km").value;
+        var fasciaEta = document.getElementById("fascia-eta").value; */
+
+        var biglietto = document.getElementById("biglietto");
+        biglietto.style.display = "block";
+    }
+)
