c95187bed54111a2a8e172b880ae608b255e1392
Vlad <andreivladpaul@gmail.com>
2021-07-22 16:11:12 +0200

    btn Genera + btn annulla

--- js/main.js
+++ js/main.js
@@ -2,11 +2,23 @@ var btnGenera = document.getElementById("calcolaPrezzo");
 
 btnGenera.addEventListener("click",
     function() {
-       /*  var nome = document.getElementById("nome").value;
+        /* var nome = document.getElementById("nome").value;
         var km = document.getElementById("km").value;
         var fasciaEta = document.getElementById("fascia-eta").value; */
 
         var biglietto = document.getElementById("biglietto");
         biglietto.style.display = "block";
+    }
+)
+
+btnAnnulla = document.getElementById("anullaPrezzo");
+btnAnnulla.addEventListener("click",
+    function() {
+        var biglietto = document.getElementById("biglietto");
+        biglietto.style.display = "none";
+
+        
+        document.getElementById("km").value = " ";
+        document.getElementById("fascia-eta").value = " ";
     }
 )
